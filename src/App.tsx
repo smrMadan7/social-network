26b2327f53d352184c78b9f8b12bfe87795e5cb8
vellaiyan <vellaiyan211464@gmail.com>
2023-02-24 19:48:58 +0530

    remove react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import SignIn from "./components/SignIn/SignIn";
 import Router from "./routes/Router";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { UserContextProvider } from "./context/UserContextProvider";
-// const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
