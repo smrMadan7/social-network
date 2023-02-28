636752fd1c77b2a0779bee001d4de99e0d20956c
vellaiyan <vellaiyan211464@gmail.com>
2023-02-28 09:56:08 +0530

    sign-in & signup integration

--- src/containers/SecureLayout.tsx
+++ src/containers/SecureLayout.tsx
@@ -69,7 +69,7 @@ const Layout = () => {
           }
         })
         .catch((error) => {
-          navigate("/home");
+          navigate("/sign-in");
         });
     };
     getCurrentUser();
