20e7a315fdd53f580501c1637e8cc686c5ed679c
vellaiyan <vellaiyan211464@gmail.com>
2023-03-05 14:37:44 +0530

    Review changes

--- src/components/Cards/Feeds.tsx
+++ src/components/Cards/Feeds.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
-import { AiTwotoneHeart } from "react-icons/ai";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { BsArrowLeftRight, BsHeart } from "react-icons/bs";
+import { GrFormClose } from "react-icons/gr";
 import { MdVerified } from "react-icons/md";
 import { TbMessage } from "react-icons/tb";
-import { ipfsGateway } from "../../constants/AppConstants";
-import { useUserContext } from "../../context/UserContextProvider";
+import { ipfsGateway, likeApi } from "../../constants/AppConstants";
+import PostProfile from "./PostProfile";
 
-const Post = (post: any) => {
+const Feeds = (post: any) => {
   const [postDetails, setPostDetails] = useState<any>();
-  const { appState }: any = useUserContext();
+  const [like, setLike] = useState(post?.post?.likes);
+  const [postProfile, setPostProfile] = useState(false);
+  const [likeCount, setLikeCount] = useState(1);
 
-  const userImageUrl = `${ipfsGateway}${appState?.action?.user?.profilePictureUrl}`;
+  const userImageUrl = `${ipfsGateway}${post?.post?.profilePictureUrl}`;
+
   const date = new Date(post?.post?.timestamp);
   const convertedDate = date.toLocaleString();
 
@@ -19,6 +22,7 @@ const Post = (post: any) => {
   myHeaders.append("Content-Type", "application/json");
 
   useEffect(() => {
+    setPostProfile(false);
     fetch(`${ipfsGateway}${post?.post?.postURI}`, {})
       .then((response) => response.json())
       .then((result) => {
@@ -29,34 +33,74 @@ const Post = (post: any) => {
       .catch((error) => {
         console.log("error in new post is ", error);
       });
-  });
+  }, []);
+
+  const incrementLike = (e: any, postData: any) => {
+    setLikeCount(likeCount + 1);
+    console.log("like count is ", likeCount);
+    const postIdData = {
+      postId: postData?.post?.postId,
+    };
+    var requestOptions: any = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify(postIdData),
+      redirect: "follow",
+    };
+
+    fetch(likeApi, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setLike(post?.post?.likes + likeCount);
+      })
+      .catch((error) => {});
+  };
 
   return (
     <>
       {postDetails ? (
         <>
+          <div className="">
+            {postProfile && (
+              <div className="w-100 fixed z-10  top-0 bottom-0 right-0 left-0 items-center m-auto h-screen bg-blackOverlay ">
+                <div className="text-white flex items-center justify-center flex m-auto h-screen">
+                  <div className=" w-70 md:w-50 h-3/6 border rounded-lg text-black bg-white">
+                    <div className="flex justify-between p-3 ">
+                      <p className="text-xl font-bold">User Details</p>
+                      <div
+                        className="px-1 py-1 rounded-full cursor-pointer hover:bg-gray-300"
+                        onClick={() => {
+                          setPostProfile(false);
+                        }}
+                      >
+                        <GrFormClose color="black" fontSize={25} />
+                      </div>
+                    </div>
+                    <div className="border"></div>
+                  </div>
+                  <PostProfile postDetails={postDetails} post={post} />
+                </div>
+              </div>
+            )}
+            <div className=""></div>
+          </div>
           <div className="p-5 flex flex-col border-b rounded-t-lg bg-white hover:bg-slate-100 w-full cursor-pointer">
             <div className="flex justify-between">
               <div className="flex gap-2">
                 <img
-                  alt="user-profile"
                   src={userImageUrl}
                   height={50}
                   width={50}
                   className=" rounded-full"
+                  onClick={() => setPostProfile(true)}
                 ></img>
                 <div className="flex flex-col">
                   <div className="flex items-center gap-1 text-center">
-                    <p className="text-lg">
-                      {appState?.action?.user?.firstName}
-                      {appState?.action?.user?.organizationName}
-                    </p>
+                    <p className="text-lg userid-background">@{post?.post?.handle}</p>
                     <MdVerified fontSize={18} color="blue" />
                   </div>
                   <div className="flex gap-2 items-center text-center">
-                    <p className="text-sm userid-background font-bold ">
-                      @{appState?.action?.user?.handle} .
-                    </p>
+                    {/* <p className="text-sm userid-background font-bold ">@{post?.post?.handle} .</p> */}
 
                     <p className="text-sm ">{convertedDate}</p>
                   </div>
@@ -71,44 +115,37 @@ const Post = (post: any) => {
                 </div>
               </div>
             </div>
+
             <div
               className="description-container"
               dangerouslySetInnerHTML={{ __html: postDetails?.content }}
             ></div>
             {postDetails?.media[0]?.file && (
               <div className="description-container" style={{ width: "200px", height: "200px" }}>
-                <img
-                  alt="post-image"
-                  src={`${ipfsGateway}${postDetails?.media[0]?.file}`}
-                  height="100"
-                ></img>
+                <img src={`${ipfsGateway}${postDetails?.media[0]?.file}`} height="100"></img>
               </div>
             )}
+
             <div className=" flex gap-7 bottom-menu-container items-center">
               <div className=" px-2 py-2 ">
-                <TbMessage fontSize={18} className="text-indigo-200  " />{" "}
+                <TbMessage fontSize={18} className="text-indigo-300  rounded-full" />{" "}
               </div>
               <div className="flex items-center">
                 <div className=" px-2 py-2 ">
-                  <BsArrowLeftRight fontSize={18} className="text-violet-200" />
+                  <BsArrowLeftRight fontSize={18} className="text-violet-300 rounded-full " />
                 </div>
                 <p className="text-sm text-violet-700 ">{post?.chat?.mirrors}</p>
               </div>
 
               <div className="flex items-center text-center">
-                <div className="rounded-full hover:bg-fuchsia-200 px-2  py-2 text-fuchsia-500 flex justify-center items-center gap-1 m-auto">
-                  {post?.post?.likes > 0 ? (
-                    <>
-                      <AiTwotoneHeart fontSize={15} className="text-fuchsia-500 mt-1" />
-                    </>
-                  ) : (
-                    <>
-                      <BsHeart fontSize={15} className="text-fuchsia-500 mt-1 " />
-                    </>
-                  )}
-                  <p>{post?.post?.likes}</p>
+                <div
+                  className="rounded-full hover:bg-fuchsia-200 px-2  py-2 flex items-center gap-1 text-fuchsia-500"
+                  onClick={(e) => incrementLike(e, post)}
+                >
+                  <BsHeart fontSize={15} className="text-fuchsia-500 mt-1 " />
+                  {like}
                 </div>
-                <p className="text-sm text-fuchsia-700 ">{post?.chat?.likes}</p>
+                {/* <p className="text-sm text-fuchsia-700 ">{like}</p> */}
               </div>
             </div>
             <style>
@@ -142,4 +179,4 @@ const Post = (post: any) => {
   );
 };
 
-export default Post;
+export default Feeds;
