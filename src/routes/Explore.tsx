0f9c9559430197e4de2f1d5aad3db82fea6c2f9c
vellaiyan <vellaiyan211464@gmail.com>
2023-03-09 16:33:04 +0530

    Review Changes

--- src/routes/Explore.tsx
+++ src/routes/Explore.tsx
@@ -36,7 +36,6 @@ const Explore = () => {
       .then((result) => {
         if (result.status !== false) {
           const feeds = result.data;
-          console.log("result is ", result.data);
           setFeeds({
             feeds,
           });
@@ -48,48 +47,57 @@ const Explore = () => {
   };
 
   return (
-    <div className="home-container flex flex-col w-full overflow-y-auto ">
-      <div style={{ height: "90px" }}></div>
-      <div className=" featured w-full border-b">
-        <div className="m-auto flex w-full px-5 py-8 text-center sm:py-20 sm:text-left">
-          <div className="flex flex-col space-y-3">
-            <div className="text-3xl font-900 sm:text-4xl">Protocol Labs Social Network👋</div>
-            <div className="sm:w-70">
-              The Protocol Labs Network drives breakthroughs in computing to push humanity forward.
+    <>
+      <div
+        className="relative home-container h-screen  flex flex-col w-full overflow-y-auto "
+        style={{ paddingTop: "90px" }}
+      >
+        <div className=" featured w-full border-b">
+          <div className="m-auto flex w-full px-5 py-8 text-center sm:py-20 sm:text-left">
+            <div className="flex flex-col space-y-3">
+              <div className="text-3xl font-900 sm:text-4xl">Protocol Labs Social Network👋</div>
+              <div className="sm:w-70">
+                The Protocol Labs Network drives breakthroughs in computing to push humanity
+                forward.
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex gap-9 items-center text-gray-700 px-5 mt-6">
-        <button className="flex gap-2 items-center p-2 rounded-lg bg-bgActive">
-          <BiMenu fontSize={20} />
-          Timeline
-        </button>
-        <button
-          className="flex gap-2  items-center p-2 rounded-lg hover:bg-violet-200 d-none"
-          onClick={() => getAllFeeds()}
-        >
-          <AiOutlineReload fontSize={23} />
-          Reload
-        </button>
-      </div>
-
-      <div className=" gap-6 mt-6 flex bg-white w-full px-5">
-        <div className="w-full md:w-70 border rounded-lg ">
-          {feeds?.action?.feeds?.map((post: any, index: number) => {
-            return <Post post={post} key={index + uuidv4()} />;
-          })}
+        <div className="flex gap-9 items-center text-gray-700 px-5 mt-6">
+          <button className="flex gap-2 items-center p-2 rounded-lg bg-bgActive">
+            <BiMenu fontSize={20} />
+            Timeline
+          </button>
+          <button
+            className="flex gap-2  items-center p-2 rounded-lg hover:bg-violet-200 d-none"
+            onClick={() => getAllFeeds()}
+          >
+            <AiOutlineReload fontSize={23} />
+            Reload
+          </button>
         </div>
-        <div className="flex-col hidden md:flex md:w-30">
-          <div className="border-orange-300	text-yellow-600 w-full bg-amber-100 rounded-lg">
-            <Notification
-              headerImg={<MdOutlineScience />}
-              title={"Beta Warning"}
-              description={
-                "This Decentralized social network is still in the beta phase, things may break, please handle us with care."
-              }
-            />
+
+        <div className=" gap-6 mt-6 flex bg-white w-full px-5 feeds-container">
+          <div className="w-full md:w-70 border rounded-lg ">
+            {feeds?.action?.feeds?.map((post: any, index: number) => {
+              return <Post post={post} key={index + uuidv4()} />;
+            })}
+          </div>
+
+          <div
+            className="flex-col hidden md:flex md:w-30 sticky top-4 right-0 z-1"
+            style={{ left: "70vw" }}
+          >
+            <div className="border-orange-300	text-yellow-600 w-full bg-amber-100 rounded-lg">
+              <Notification
+                headerImg={<MdOutlineScience />}
+                title={"Beta Warning"}
+                description={
+                  "This Decentralized social network is still in the beta phase, things may break, please handle us with care."
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -125,7 +133,7 @@ const Explore = () => {
   
         `}
       </style>
-    </div>
+    </>
   );
 };
 
