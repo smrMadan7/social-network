4529ceb4112aec5d2b09ab1cff6f2aa2a2d84aca
vellaiyan <vellaiyan211464@gmail.com>
2023-03-23 16:22:45 +0530

    Code splitting

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,14 @@
 import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Explore from "./Explore";
+import Home from "./Home";
+
 const Member = lazy(() => import("../components/Forms/Member"));
-const Home = lazy(() => import("./Home"));
 const Team = lazy(() => import("../components/Forms/Team"));
 const MemberProfile = lazy(() => import("../components/Profile/MemberProfile"));
 const TeamProfile = lazy(() => import("../components/Profile/TeamProfile"));
-const SecureLayout = lazy(() => import("../containers/SecureLayout"));
 const Register = lazy(() => import("../Register/Register"));
-const Explore = lazy(() => import("./Explore"));
+const SecureLayout = lazy(() => import("../containers/SecureLayout"));
 
 const Router = () => {
   return (
